0b4291695dde46431d63865b96c1cb241ec024dc
leejongchan <jongchanlee93@gmail.com>
2019-07-06 00:16:09 +0900

    changeTimeString 수정

--- src/library/changeTimeString.js
+++ src/library/changeTimeString.js
@@ -5,11 +5,11 @@ moment.locale('ko');
 // 리뷰 작성 시간 스트링 생성
 function makeReviewTimeString(writingTime) {
   const currentTime = moment();
-  const momentWritingTime = moment(writingTime).format('YYYY.MM.DD HH:mm:ss');
+  const momentWritingTime = moment(writingTime).format('YYYY-MM-DD HH:mm:ss');
   const timeDiff = moment.duration(currentTime.diff(momentWritingTime)).asHours();
 
   if (timeDiff > 24) {
-    return momentWritingTime;
+    return momentWritingTime.replace(/-/g, '.');
   }
 
   return moment(momentWritingTime).fromNow();
